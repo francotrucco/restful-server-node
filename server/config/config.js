939ff3f04d0927a06cae17935c3db4911fdaf7bf
Franco Trucco <franco.trucco1@gmail.com>
2018-10-04 12:46:45 -0300

    Adds env var

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDb;
 if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDb = 'mongodb://admin:123456a@ds121665.mlab.com:21665/cafe';
+    urlDb = process.env.MONGO_URI;
 }
 
 process.env.urlDb = urlDb;
